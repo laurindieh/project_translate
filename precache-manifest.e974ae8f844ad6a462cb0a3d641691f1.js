810f055a86889c622b7837a37696da1b02938d08
laurindieh <118126926+laurindieh@users.noreply.github.com>
2024-10-03 22:21:13 +0300

    Add files via upload

---
+++ precache-manifest.e974ae8f844ad6a462cb0a3d641691f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bd60d42e186039f529268553319eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5008a7d60f533611c2dd",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "5008a7d60f533611c2dd",
+    "url": "/static/js/main.f7e3485b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
